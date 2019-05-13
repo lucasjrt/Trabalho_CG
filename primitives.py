a59c54912dc244acff763fe56e71c5805c7e6381
Brenocsc <42703380+Brenocsc@users.noreply.github.com>
2019-05-13 10:55:35 -0300

    Add files via upload

--- primitives.py
+++ primitives.py
@@ -20,12 +20,21 @@ screen.fill(background)
 layer.fill(white)
 pygame.display.flip()
 
-def colorir(x, y, color, overlay=screen):
-    overlay.set_at((x,y),color)
-    if(overlay.get_at((x,y-1)) == white): colorir(x,y-1,color)
-    if(overlay.get_at((x+1,y)) == white and screen_size[0] > x): colorir(x+1,y,color)
-    if(overlay.get_at((x,y+1)) == white and screen_size[1] > y): colorir(x,y+1,color)
-    if(overlay.get_at((x-1,y)) == white): colorir(x-1,y,color)
+def colorir(x, y, cor, overlay=screen):
+    corRecursao(x,y,cor,overlay.get_at((x,y)))
+    #pygame.display.flip()
+
+def corRecursao(x, y, cor, corAnt, overlay=screen):
+    overlay.set_at((x,y),cor)
+    '''if(overlay.get_at((x-1,y-1)) == white): corRecursao(x-1,y-1,color)
+    if(overlay.get_at((x+1,y-1)) == white): corRecursao(x+1,y-1,color)
+    if(overlay.get_at((x-1,y-1)) == white): corRecursao(x-1,y-1,color)
+    if(overlay.get_at((x+1,y-1)) == white): corRecursao(x+1,y-1,color)'''
+    
+    if(overlay.get_at((x,y-1)) == corAnt): corRecursao(x,y-1,cor,corAnt)
+    if(overlay.get_at((x+1,y)) == corAnt and screen_size[0] > x): corRecursao(x+1,y,cor,corAnt)
+    if(overlay.get_at((x,y+1)) == corAnt and screen_size[1] > y): corRecursao(x,y+1,cor,corAnt)
+    if(overlay.get_at((x-1,y)) == corAnt): corRecursao(x-1,y,cor,corAnt)
 
 def _linhaH(x0, y0, x1, y1, color=foreground, overlay=screen):
     dx = x1 - x0
@@ -121,6 +130,60 @@ def triangulo(x, y, size, color=foreground, overlay=screen):
     _segmento(x + size, y + size, x - size, y + size, color, overlay)
     pygame.display.flip()
 
+def compute_bezier_points(vertices, numPoints=None):
+    if numPoints is None:
+        numPoints = 30
+    if numPoints < 2 or len(vertices) != 4:
+        return None
+
+    result = []
+
+    b0x = vertices[0][0]
+    b0y = vertices[0][1]
+    b1x = vertices[1][0]
+    b1y = vertices[1][1]
+    b2x = vertices[2][0]
+    b2y = vertices[2][1]
+    b3x = vertices[3][0]
+    b3y = vertices[3][1]
+
+    # Compute polynomial coefficients from Bezier points
+    ax = -b0x + 3 * b1x + -3 * b2x + b3x
+    ay = -b0y + 3 * b1y + -3 * b2y + b3y
+    bx = 3 * b0x + -6 * b1x + 3 * b2x
+    by = 3 * b0y + -6 * b1y + 3 * b2y
+    cx = -3 * b0x + 3 * b1x
+    cy = -3 * b0y + 3 * b1y
+    dx = b0x
+    dy = b0y
+
+    # Set up the number of steps and step size
+    numSteps = numPoints - 1 # arbitrary choice
+    h = 1.0 / numSteps # compute our step size
+
+    # Compute forward differences from Bezier points and "h"
+    pointX = dx
+    pointY = dy
+    firstFDX = ax * (h * h * h) + bx * (h * h) + cx * h
+    firstFDY = ay * (h * h * h) + by * (h * h) + cy * h
+    secondFDX = 6 * ax * (h * h * h) + 2 * bx * (h * h)
+    secondFDY = 6 * ay * (h * h * h) + 2 * by * (h * h)
+    thirdFDX = 6 * ax * (h * h * h)
+    thirdFDY = 6 * ay * (h * h * h)
+
+    # Compute points at each step
+    result.append((int(pointX), int(pointY)))
+    
+    for i in range(numSteps):
+        pointX += firstFDX
+        pointY += firstFDY
+        firstFDX += secondFDX
+        firstFDY += secondFDY
+        secondFDX += thirdFDX
+        secondFDY += thirdFDY
+        result.append((int(pointX), int(pointY)))
+    print(result)
+    return result
 
 '''def curva(x, y, x0, y0, color=foreground, overlay=screen):
 	for t in numpy.arange(0,1,0.01):
