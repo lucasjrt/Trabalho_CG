e8ebe7448d51921da18caf8234098a9ba0b1a810
Lucasjrt <lucasjrt@outlook.com>
2019-05-13 20:29:25 -0300

    Colorir e trocando espacos por tab

--- primitives.py
+++ primitives.py
@@ -21,181 +21,203 @@ layer.fill(white)
 pygame.display.flip()
 
 def colorir(x, y, cor, overlay=screen):
-    corRecursao(x,y,cor,overlay.get_at((x,y)))
-    #pygame.display.flip()
+	stack = [(x,y)]
+	atual = (x,y)
+	color = overlay.get_at((x,y))
+	while stack:
+		overlay.set_at((stack[-1]), cor)
+		atual = stack[-1]
+		stack.pop()
+		if atual[0] < screen_size[0] - 1 and atual[1] < screen_size[1] - 1 and overlay.get_at((atual[0] + 1, atual[1])) == color:
+			stack.append((atual[0] + 1, atual[1]))
 
-def corRecursao(x, y, cor, corAnt, overlay=screen):
-    overlay.set_at((x,y),cor)
-    '''if(overlay.get_at((x-1,y-1)) == white): corRecursao(x-1,y-1,color)
-    if(overlay.get_at((x+1,y-1)) == white): corRecursao(x+1,y-1,color)
-    if(overlay.get_at((x-1,y-1)) == white): corRecursao(x-1,y-1,color)
-    if(overlay.get_at((x+1,y-1)) == white): corRecursao(x+1,y-1,color)'''
-    
-    if(overlay.get_at((x,y-1)) == corAnt): corRecursao(x,y-1,cor,corAnt)
-    if(overlay.get_at((x+1,y)) == corAnt and screen_size[0] > x): corRecursao(x+1,y,cor,corAnt)
-    if(overlay.get_at((x,y+1)) == corAnt and screen_size[1] > y): corRecursao(x,y+1,cor,corAnt)
-    if(overlay.get_at((x-1,y)) == corAnt): corRecursao(x-1,y,cor,corAnt)
+		if atual[0] < screen_size[0] - 1 and atual[1] < screen_size[1] - 1 and overlay.get_at((atual[0] - 1, atual[1])) == color:
+			stack.append((atual[0] - 1, atual[1]))
+
+		if atual[0] < screen_size[0] - 1 and atual[1] < screen_size[1] - 1 and overlay.get_at((atual[0], atual[1] + 1)) == color:
+			stack.append((atual[0], atual[1] + 1))
+
+		if atual[0] < screen_size[0] - 1 and atual[1] < screen_size[1] - 1 and overlay.get_at((atual[0], atual[1] - 1)) == color:
+			stack.append((atual[0], atual[1] - 1))
+	#corRecursao(x,y,cor,overlay.get_at((x,y)))
+	#pygame.display.flip()
+	
+
+#def corRecursao(x, y, cor, corAnt, overlay=screen):
+#	overlay.set_at((x,y),cor)
+	'''if(overlay.get_at((x-1,y-1)) == white): corRecursao(x-1,y-1,color)
+	if(overlay.get_at((x+1,y-1)) == white): corRecursao(x+1,y-1,color)
+	if(overlay.get_at((x-1,y-1)) == white): corRecursao(x-1,y-1,color)
+	if(overlay.get_at((x+1,y-1)) == white): corRecursao(x+1,y-1,color)'''
+
+#	if(overlay.get_at((x,y-1)) == corAnt): corRecursao(x,y-1,cor,corAnt)
+#	if(overlay.get_at((x+1,y)) == corAnt and screen_size[0] > x): corRecursao(x+1,y,cor,corAnt)
+#	if(overlay.get_at((x,y+1)) == corAnt and screen_size[1] > y): corRecursao(x,y+1,cor,corAnt)
+#	if(overlay.get_at((x-1,y)) == corAnt): corRecursao(x-1,y,cor,corAnt)
+	
 
 def _linhaH(x0, y0, x1, y1, color=foreground, overlay=screen):
-    dx = x1 - x0
-    dy = y1 - y0
-    yi = 1
-    if dy < 0:
-        yi = -1
-        dy = -dy
-    D = 2*dy - dx
-    y = y0
-    for x in range(dx):
-        overlay.set_at((x + x0, y), color)
-        if D > 0:
-           y = y + yi
-           D = D - 2*dx
-        D = D + 2*dy
+	dx = x1 - x0
+	dy = y1 - y0
+	yi = 1
+	if dy < 0:
+		yi = -1
+		dy = -dy
+	D = 2*dy - dx
+	y = y0
+	for x in range(dx):
+		overlay.set_at((x + x0, y), color)
+		if D > 0:
+			y = y + yi
+			D = D - 2*dx
+		D = D + 2*dy
 
 
 
 def _linhaV(x0, y0, x1, y1, color=foreground, overlay=screen):
-    dx = x1 - x0
-    dy = y1 - y0
-    xi = 1
-    if dx < 0:
-        xi = -1
-        dx = -dx
-    D = 2*dx - dy
-    x = x0
-    for y in range(dy):
-        overlay.set_at((x, y + y0), color)
-        if D > 0:
-            x = x + xi
-            D = D - 2*dy
-        D = D + 2*dx
+	dx = x1 - x0
+	dy = y1 - y0
+	xi = 1
+	if dx < 0:
+		xi = -1
+		dx = -dx
+	D = 2*dx - dy
+	x = x0
+	for y in range(dy):
+		overlay.set_at((x, y + y0), color)
+		if D > 0:
+			x = x + xi
+			D = D - 2*dy
+		D = D + 2*dx
 
 
 def _segmento(x0, y0, x1, y1, color=foreground, overlay=screen):
-    if abs(y1 - y0) < abs(x1 - x0):
-        if x0 > x1:
-            _linhaH(x1, y1, x0, y0, color, overlay)
-        else:
-            _linhaH(x0, y0, x1, y1, color, overlay)
-    else:
-        if y0 > y1:
-            _linhaV(x1, y1, x0, y0, color, overlay)
-        else:
-            _linhaV(x0, y0, x1, y1, color, overlay)
+	if abs(y1 - y0) < abs(x1 - x0):
+		if x0 > x1:
+			_linhaH(x1, y1, x0, y0, color, overlay)
+		else:
+			_linhaH(x0, y0, x1, y1, color, overlay)
+	else:
+		if y0 > y1:
+			_linhaV(x1, y1, x0, y0, color, overlay)
+		else:
+			_linhaV(x0, y0, x1, y1, color, overlay)
 
+#linha(x, y, x, y, cor, layer)
 
-def linha(x0, y0, x1, y1, color):
-    _segmento(x0, y0, x1, y1, color)
-    pygame.display.flip()
+def linha(x0, y0, x1, y1, color, overlay=screen):
+	_segmento(x0, y0, x1, y1, color, overlay)
+	pygame.display.flip()
 
 
 def circulo(x0, y0, r, color=foreground, overlay=screen):
-    x = 0
-    y = r
-    d = 1 - r
-    _circulo (x, y, x0, y0, color, screen)
-    while y > x :
-        if d < 0 :
-            d = d + ( 2 * x ) + 3
-        else:
-            d = d + 2 * ( x - y ) + 5
-            y = y - 1
-        x = x + 1
-        _circulo(x, y, x0, y0, color, overlay)
-    pygame.display.flip()
-    
+	x = 0
+	y = r
+	d = 1 - r
+	_circulo (x, y, x0, y0, color, screen)
+	while y > x :
+		if d < 0 :
+			d = d + ( 2 * x ) + 3
+		else:
+			d = d + 2 * ( x - y ) + 5
+			y = y - 1
+		x = x + 1
+		_circulo(x, y, x0, y0, color, overlay)
+	pygame.display.flip()
+
 
 def _circulo(x, y, x0, y0, color=foreground, overlay=screen):
-    overlay.set_at((x + x0, y + y0), color)
-    overlay.set_at((x0 - y, x + y0), color)
-    overlay.set_at((x0 - y, y0 - x), color)
-    overlay.set_at((x0 - x, y0 - y), color)
-    overlay.set_at((x0 - x, y + y0), color)
-    overlay.set_at((x + x0, y0 - y), color)
-    overlay.set_at((y + x0, y0 - x), color)
-    overlay.set_at((y + x0, x + y0), color)
+	overlay.set_at((x + x0, y + y0), color)
+	overlay.set_at((x0 - y, x + y0), color)
+	overlay.set_at((x0 - y, y0 - x), color)
+	overlay.set_at((x0 - x, y0 - y), color)
+	overlay.set_at((x0 - x, y + y0), color)
+	overlay.set_at((x + x0, y0 - y), color)
+	overlay.set_at((y + x0, y0 - x), color)
+	overlay.set_at((y + x0, x + y0), color)
 
 
 def retangulo(x, y, w, h, color=foreground, overlay=screen):
-    _segmento(x, y, w, y, color, overlay)
-    _segmento(w, y, w, h, color, overlay)
-    _segmento(w, h, x, h, color, overlay)
-    _segmento(x, h, x, y, color, overlay)
-    pygame.display.flip()
+	_segmento(x, y, w, y, color, overlay)
+	_segmento(w, y, w, h, color, overlay)
+	_segmento(w, h, x, h, color, overlay)
+	_segmento(x, h, x, y, color, overlay)
+	pygame.display.flip()
 
 
 def triangulo(x, y, size, color=foreground, overlay=screen):
-    _segmento(x - size, y + size, x, y - size, color, overlay)
-    _segmento(x, y - size, x + size,  y + size, color, overlay)
-    _segmento(x + size, y + size, x - size, y + size, color, overlay)
-    pygame.display.flip()
+	_segmento(x - size, y + size, x, y - size, color, overlay)
+	_segmento(x, y - size, x + size,  y + size, color, overlay)
+	_segmento(x + size, y + size, x - size, y + size, color, overlay)
+	pygame.display.flip()
+
 
 def compute_bezier_points(vertices, numPoints=None):
-    if numPoints is None:
-        numPoints = 30
-    if numPoints < 2 or len(vertices) != 4:
-        return None
+	if numPoints is None:
+		numPoints = 30
+	if numPoints < 2 or len(vertices) != 4:
+		return None
 
-    result = []
+	result = []
 
-    b0x = vertices[0][0]
-    b0y = vertices[0][1]
-    b1x = vertices[1][0]
-    b1y = vertices[1][1]
-    b2x = vertices[2][0]
-    b2y = vertices[2][1]
-    b3x = vertices[3][0]
-    b3y = vertices[3][1]
+	b0x = vertices[0][0]
+	b0y = vertices[0][1]
+	b1x = vertices[1][0]
+	b1y = vertices[1][1]
+	b2x = vertices[2][0]
+	b2y = vertices[2][1]
+	b3x = vertices[3][0]
+	b3y = vertices[3][1]
 
-    # Compute polynomial coefficients from Bezier points
-    ax = -b0x + 3 * b1x + -3 * b2x + b3x
-    ay = -b0y + 3 * b1y + -3 * b2y + b3y
-    bx = 3 * b0x + -6 * b1x + 3 * b2x
-    by = 3 * b0y + -6 * b1y + 3 * b2y
-    cx = -3 * b0x + 3 * b1x
-    cy = -3 * b0y + 3 * b1y
-    dx = b0x
-    dy = b0y
+	# Compute polynomial coefficients from Bezier points
+	ax = -b0x + 3 * b1x + -3 * b2x + b3x
+	ay = -b0y + 3 * b1y + -3 * b2y + b3y
+	bx = 3 * b0x + -6 * b1x + 3 * b2x
+	by = 3 * b0y + -6 * b1y + 3 * b2y
+	cx = -3 * b0x + 3 * b1x
+	cy = -3 * b0y + 3 * b1y
+	dx = b0x
+	dy = b0y
 
-    # Set up the number of steps and step size
-    numSteps = numPoints - 1 # arbitrary choice
-    h = 1.0 / numSteps # compute our step size
+	# Set up the number of steps and step size
+	numSteps = numPoints - 1 # arbitrary choice
+	h = 1.0 / numSteps # compute our step size
 
-    # Compute forward differences from Bezier points and "h"
-    pointX = dx
-    pointY = dy
-    firstFDX = ax * (h * h * h) + bx * (h * h) + cx * h
-    firstFDY = ay * (h * h * h) + by * (h * h) + cy * h
-    secondFDX = 6 * ax * (h * h * h) + 2 * bx * (h * h)
-    secondFDY = 6 * ay * (h * h * h) + 2 * by * (h * h)
-    thirdFDX = 6 * ax * (h * h * h)
-    thirdFDY = 6 * ay * (h * h * h)
+	# Compute forward differences from Bezier points and "h"
+	pointX = dx
+	pointY = dy
+	firstFDX = ax * (h * h * h) + bx * (h * h) + cx * h
+	firstFDY = ay * (h * h * h) + by * (h * h) + cy * h
+	secondFDX = 6 * ax * (h * h * h) + 2 * bx * (h * h)
+	secondFDY = 6 * ay * (h * h * h) + 2 * by * (h * h)
+	thirdFDX = 6 * ax * (h * h * h)
+	thirdFDY = 6 * ay * (h * h * h)
 
-    # Compute points at each step
-    result.append((int(pointX), int(pointY)))
-    
-    for i in range(numSteps):
-        pointX += firstFDX
-        pointY += firstFDY
-        firstFDX += secondFDX
-        firstFDY += secondFDY
-        secondFDX += thirdFDX
-        secondFDY += thirdFDY
-        result.append((int(pointX), int(pointY)))
-    print(result)
-    return result
+	# Compute points at each step
+	result.append((int(pointX), int(pointY)))
+	
+	for i in range(numSteps):
+		pointX += firstFDX
+		pointY += firstFDY
+		firstFDX += secondFDX
+		firstFDY += secondFDY
+		secondFDX += thirdFDX
+		secondFDY += thirdFDY
+		result.append((int(pointX), int(pointY)))
+	print(result)
+	return result
 
 '''def curva(x, y, x0, y0, color=foreground, overlay=screen):
 	for t in numpy.arange(0,1,0.01):
-    omt  = 1-t
-    omt2 = omt*omt	
-    omt3 = omt2*omt		
-    t2   = t*t
-    t3   = t2*t
-    x    = omt3 * p1[0] + ((3*omt2)*t*p1[0]) + (3*omt*t2*p3[0])+t3*p4[0]
-    y    = omt3 * p1[1] + ((3*omt2)*t*p1[1]) + (3*omt*t2*p3[1])+t3*p4[1]
-    x    = int(numpy.floor(x))
-    y    = int(numpy.floor(y))
-     
-    screen.set_at((x,y), white)
-    pygame.display.flip()'''
+	omt  = 1-t
+	omt2 = omt*omt	
+	omt3 = omt2*omt		
+	t2   = t*t
+	t3   = t2*t
+	x    = omt3 * p1[0] + ((3*omt2)*t*p1[0]) + (3*omt*t2*p3[0])+t3*p4[0]
+	y    = omt3 * p1[1] + ((3*omt2)*t*p1[1]) + (3*omt*t2*p3[1])+t3*p4[1]
+	x    = int(numpy.floor(x))
+	y    = int(numpy.floor(y))
+	 
+	screen.set_at((x,y), white)
+	pygame.display.flip()'''
--- testWindow.py
+++ testWindow.py
@@ -23,12 +23,12 @@ c = 0
 linha(screen_size[0] >> 2, 0, screen_size[0] >> 2, screen_size[1], foreground)#viewport left
 linha(screen_size[0] >> 2, screen_size[1] >> 4,screen_size[0], screen_size[1] >> 4,foreground)# viewport top
 for i in range((screen_size[0] >> 2) + 10, (screen_size[0] >> 2) + 538, 33):
-    retangulo(i, 3, i+29, 32, foreground)
-    colorir(i+1,4,cores[c])
-    c = c+1
+	retangulo(i, 3, i+29, 32, foreground)
+	colorir(i+1,4,cores[c])
+	c = c+1
 for i in range(len(primitivas)):
-    y = screen_size[1] // len(primitivas) * (i + 1)
-    linha(0, y, screen_size[0] >> 2, y, foreground)
+	y = screen_size[1] // len(primitivas) * (i + 1)
+	linha(0, y, screen_size[0] >> 2, y, foreground)
 retangulo((screen_size[0] >> 2) + 556, 5, (screen_size[0] >> 2) + 581, 30,foreground)
 
 #Desenho amostral das primitivas
@@ -75,148 +75,144 @@ layer.blit(screen, (0,0))
 
 #Mouse listeners
 def on_move(x, y):
-    global control_points
-    global start
-    global atual
-    mouseX, mouseY = pygame.mouse.get_pos()
-    if clicked:
-        if mouseX < screen_size[0] >> 2:
-            mouseX = screen_size[0] >> 2
-        if mouseY < screen_size[1] >> 4:
-            mouseY = screen_size[1] >> 4
-        screen.blit(layer, (0,0))
-        if atual == 0: 
-            linha(start[0], start[1], mouseX, mouseY, foreground)
-        elif atual == 1:
-            r = int(math.sqrt(((mouseX - start[0]) ** 2) + ((mouseY - start[1]) ** 2)))
-            if r > start[0] - (screen_size[0] >> 2):
-                r = start[0] - (screen_size[0] >> 2)
-            if r > start[1] - (screen_size[1] >> 4):
-                r = start[1] - (screen_size[1] >> 4)
-            circulo(start[0], start[1], r, foreground)
-        elif atual == 2:
-            retangulo(start[0], start[1], mouseX, mouseY, foreground)
-        elif atual == 3:
-                
-            signal = abs(mouseY - start[1]) // (mouseY - start[1]) if mouseY != start[1] else 1
-            if mouseX < start[0]:
-                signal = -signal
-
-            if abs(mouseX - start[0]) > abs(start[1] - (screen_size[1] >> 4)) and start[1] > mouseY:
-                if mouseX > start[0]:
-                     mouseX = start[0] + abs((screen_size[1] >> 4) - start[1])
-                else:
-                     mouseX = start[0] - abs((screen_size[1] >> 4) - start[1])    
-            retangulo(start[0], start[1], mouseX, start[1] + ((mouseX - start[0]) * signal), foreground)
-        elif atual == 4:
-          	linha(start[0], start[1], mouseX, mouseY, foreground)
-        elif atual == 5:
-            for p in control_points:
-                if abs(p[0] - start[0]) < 10 and abs(p[1] - start[1]) < 10:
-                    print("chego aqui")
-                    print(selected_point)
-                    if selected_point is not None:
-                        control_points[selected_point] = (mouseX, mouseY)
-                        print(control_points)
-                        pygame.draw.circle(screen, (0,255,0), (control_points[selected_point][0],control_points[selected_point][1]), 10)
-                
-                    # Desenha os pontos de controle
-                    for p in control_points:
-                        circulo(p[0], p[1], 4, (0,0,255))
-
-                    # Desenha as linhas de controle
-                    #pygame.draw.lines(screen, (200,200,200), False, [(x[0], x[1]) for x in control_points])
-                    for i in range(0,len(control_points)-1):
-                        linha(control_points[i][0], control_points[i][1], control_points[i+1][0], control_points[i+1][1], (200,200,200))
-
-                    # Desenha a curva de bezier
-                    b_points = compute_bezier_points([(x[0], x[1]) for x in control_points])
-                    for i in range(0,len(b_points)-1):
-                        linha(b_points[i][0], b_points[i][1], b_points[i+1][0], b_points[i+1][1], (255,0,0))
-                    pygame.display.flip()
-        else:
-            pass
-            #floodfill
+	global control_points
+	global start
+	global atual
+	mouseX, mouseY = pygame.mouse.get_pos()
+	if clicked:
+		if mouseX < screen_size[0] >> 2:
+			mouseX = screen_size[0] >> 2
+		if mouseY < screen_size[1] >> 4:
+			mouseY = screen_size[1] >> 4
+		screen.blit(layer, (0,0))
+		if atual == 0: 
+			linha(start[0], start[1], mouseX, mouseY, foreground)
+		elif atual == 1:
+			r = int(math.sqrt(((mouseX - start[0]) ** 2) + ((mouseY - start[1]) ** 2)))
+			if r > start[0] - (screen_size[0] >> 2):
+				r = start[0] - (screen_size[0] >> 2)
+			if r > start[1] - (screen_size[1] >> 4):
+				r = start[1] - (screen_size[1] >> 4)
+			circulo(start[0], start[1], r, foreground)
+		elif atual == 2:
+			retangulo(start[0], start[1], mouseX, mouseY, foreground)
+		elif atual == 3:
+			signal = abs(mouseY - start[1]) // (mouseY - start[1]) if mouseY != start[1] else 1
+			if mouseX < start[0]:
+				signal = -signal
+			if abs(mouseX - start[0]) > abs(start[1] - (screen_size[1] >> 4)) and start[1] > mouseY:
+				if mouseX > start[0]:
+					mouseX = start[0] + abs((screen_size[1] >> 4) - start[1])
+				else:
+					mouseX = start[0] - abs((screen_size[1] >> 4) - start[1])    
+			retangulo(start[0], start[1], mouseX, start[1] + ((mouseX - start[0]) * signal), foreground)
+		elif atual == 4:
+			linha(start[0], start[1], mouseX, mouseY, foreground)
+		elif atual == 5:
+			for p in control_points:
+				if abs(p[0] - start[0]) and abs(p[1] - start[1]):
+					print("chego aqui")
+					print(selected_point)
+					if selected_point is not None:
+						control_points[selected_point] = (mouseX, mouseY)
+						print(control_points)
+						circulo(control_points[selected_point][0], control_points[selected_point][1], 10, (0,255,0))
+					# Desenha os pontos de controle
+					for p in control_points:
+						circulo(p[0], p[1], 4, (0,0,255))
+					# Desenha as linhas de controle
+					#pygame.draw.lines(screen, (200,200,200), False, [(x[0], x[1]) for x in control_points])
+					for i in range(0,len(control_points)-1):
+						linha(control_points[i][0], control_points[i][1], control_points[i+1][0], control_points[i+1][1], (200,200,200))
+					# Desenha a curva de bezier
+					b_points = compute_bezier_points([(x[0], x[1]) for x in control_points])
+					for i in range(0,len(b_points)-1):
+						linha(b_points[i][0], b_points[i][1], b_points[i+1][0], b_points[i+1][1], (255,0,0))
+					pygame.display.flip()
+		else:
+			pass
+			#floodfill
 
 
 def on_click(x, y, button, pressed):
-    global selected_point
-    global control_points
-    global clicked
-    global start
-    global atual
-    global atualcor
-    global curving
-    mouseX, mouseY = pygame.mouse.get_pos()
-    start = (mouseX, mouseY)
-    if pressed:
-        layer.blit(screen, (0,0))
-        if mouseX > screen_size[0] >> 2 and mouseY > screen_size[1] >> 4:
-            if atual == 5:
-                if curving:
-                    index = 0
-                    for p in control_points:
-                        if abs(p[0] - start[0]) < 10 and abs(p[1] - start[1]) < 10 :
-                            selected_point = index
-                            print(selected_point)
-                        index = index + 1
-                    clicked = True
-                else:
-                    # Desenha os pontos de controle
-                    for p in control_points:
-                        circulo(p[0], p[1], 4, (0,0,255))
+	global selected_point
+	global control_points
+	global clicked
+	global start
+	global atual
+	global atualcor
+	global curving
+	mouseX, mouseY = pygame.mouse.get_pos()
+	if pressed:
+		layer.blit(screen, (0,0))
+		#Colorir
+		if mouseX > screen_size[0] >> 2 and mouseY > screen_size[1] >> 4:
+			start = (mouseX, mouseY)
+			if atual == 5:
+				if curving:
+					index = 0
+					for p in control_points:
+						if abs(p[0] - start[0]) < 10 and abs(p[1] - start[1]) < 10 :
+							selected_point = index
+							print(selected_point)
+						index = index + 1
+					clicked = True
+				else:
+					# Desenha os pontos de controle
+					for p in control_points:
+						circulo(p[0], p[1], 4, (0,0,255))
 
-                    # Desenha as linhas de controle
-                    #pygame.draw.lines(screen, (200,200,200), False, [(x[0], x[1]) for x in control_points])
-                    for i in range(0,len(control_points)-1):
-                        linha(control_points[i][0], control_points[i][1], control_points[i+1][0], control_points[i+1][1], (200,200,200))
+					# Desenha as linhas de controle
+					#pygame.draw.lines(screen, (200,200,200), False, [(x[0], x[1]) for x in control_points])
+					for i in range(0,len(control_points)-1):
+						linha(control_points[i][0], control_points[i][1], control_points[i+1][0], control_points[i+1][1], (200,200,200))
 
-                    # Desenha a curva de bezier
-                    b_points = compute_bezier_points([(x[0], x[1]) for x in control_points])
-                    for i in range(0,len(b_points)-1):
-                        linha(b_points[i][0], b_points[i][1], b_points[i+1][0], b_points[i+1][1], (255,0,0))
+					# Desenha a curva de bezier
+					b_points = compute_bezier_points([(x[0], x[1]) for x in control_points])
+					for i in range(0,len(b_points)-1):
+						linha(b_points[i][0], b_points[i][1], b_points[i+1][0], b_points[i+1][1], (255,0,0))
 
-                    pygame.display.flip()
-                    curving = True
-            elif atual == 6:
-                print(atualcor)
-                colorir(mouseX,mouseY,cores[atualcor])
-                pygame.display.flip()
-            else:
-                clicked = True
-        elif mouseX < screen_size[0] >> 2: #Dentro do menu
-            atual = mouseY // (screen_size[1] // len(primitivas))
-        elif mouseX > (screen_size[0] >> 2) + 556 and mouseY > 5 and mouseX < (screen_size[0] >> 2) + 581 and mouseY < 30:
-            atual = 6
-            print("colorir")
-        else:
-            c = 0
-            for i in range((screen_size[0] >> 2) + 10, (screen_size[0] >> 2) + 538, 33):
-                if mouseX > i and mouseY > 3 and mouseX < i+29 and mouseY < 32:
-                    print(c)
-                    atualcor = c
-                    break
-                c = c+1
-    else:
-        layer.blit(screen, (0,0))
-        if atual != 4:
-            clicked = False
+					pygame.display.flip()
+					curving = True
+			elif atual == 6:
+				print(atualcor)
+				colorir(mouseX,mouseY,cores[atualcor])
+				pygame.display.flip()
+			else:
+				clicked = True
+		elif mouseX < screen_size[0] >> 2: #Dentro do menu
+			atual = mouseY // (screen_size[1] // len(primitivas))
+		elif mouseX > (screen_size[0] >> 2) + 556 and mouseY > 5 and mouseX < (screen_size[0] >> 2) + 581 and mouseY < 30:
+			atual = 6
+			print("colorir")
+		else:
+			c = 0
+			for i in range((screen_size[0] >> 2) + 10, (screen_size[0] >> 2) + 538, 33):
+				if mouseX > i and mouseY > 3 and mouseX < i+29 and mouseY < 32:
+					print(c)
+					atualcor = c
+					break
+				c = c+1
+	else:
+		layer.blit(screen, (0,0))
+		if atual != 4:
+			clicked = False
 
 
 def on_press(key):
-    global clicked
-    if key == Key.esc:
-        clicked = False
-        screen.blit(layer, (0,0))
+	global clicked
+	if key == Key.esc:
+		clicked = False
+		screen.blit(layer, (0,0))
 
 
 listener = mouse.Listener( on_move=on_move, on_click=on_click)
 listener.start()
 
 def kb_listener(args):
-    global on_press
-    with Listener(on_press=on_press) as l:
-        l.join()
+	global on_press
+	with Listener(on_press=on_press) as l:
+		l.join()
 
 
 thread = Thread(target = kb_listener, args = (10, ))
@@ -226,9 +222,9 @@ thread.start()
 
 #try:
 while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
 #except SystemExit:
 #    pygame.quit()
 #    thread.exit()
